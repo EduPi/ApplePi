9b06a0999174d2a12c0ff80c68269369aa8a652c
Pringels <peterringelmann@gmail.com>
2014-03-03 15:49:41 +0200

    Added preloader for all images
    Some images in number/alphabet game would not load correctly. They now
    all get preloaded at the first page load.

--- inc/nav.php
+++ inc/nav.php
@@ -5,3 +5,87 @@
     <a href="numbers.php"><img src="img/nav-3.png" width="101px" height="100px"/></a>
     <a href="drawing.php"><img src="img/nav-4.png" width="101px" height="100px"/></a>
 </div>
+
+<script>
+
+			if (document.images) {
+				img1 = new Image();
+				img2 = new Image();
+				img3 = new Image();
+				img4 = new Image();
+				img5 = new Image();
+				img6 = new Image();
+				img7 = new Image();
+				img8 = new Image();
+				img9 = new Image();
+				img10 = new Image();
+				img11 = new Image();
+				img12 = new Image();
+				img13 = new Image();
+				img14 = new Image();
+				img15 = new Image();
+				img16 = new Image();
+				img17 = new Image();
+				img18 = new Image();
+				img19 = new Image();
+				img20 = new Image();
+				img21 = new Image();
+				img22 = new Image();
+				img23 = new Image();
+				img24 = new Image();
+				img25 = new Image();
+				img26 = new Image();
+				
+				img_1 = new Image();
+				img_2 = new Image();
+				img_3 = new Image();
+				img_4 = new Image();
+				img_5 = new Image();
+				img_6 = new Image();
+				img_7 = new Image();
+				img_8 = new Image();
+				img_9 = new Image();
+				img_10 = new Image();
+
+				img1.src = "img/alphabet/pictures/a.jpg";
+				img2.src = "img/alphabet/pictures/b.jpg";
+				img3.src = "img/alphabet/pictures/c.jpg";
+				img4.src = "img/alphabet/pictures/d.jpg";
+				img5.src = "img/alphabet/pictures/e.jpg";
+				img6.src = "img/alphabet/pictures/f.jpg";
+				img7.src = "img/alphabet/pictures/g.jpg";
+				img8.src = "img/alphabet/pictures/h.jpg";
+				img9.src = "img/alphabet/pictures/i.jpg";
+				img10.src = "img/alphabet/pictures/j.jpg";
+				img11.src = "img/alphabet/pictures/k.jpg";
+				img12.src = "img/alphabet/pictures/l.jpg";
+				img13.src = "img/alphabet/pictures/m.jpg";
+				img14.src = "img/alphabet/pictures/n.jpg";
+				img15.src = "img/alphabet/pictures/o.jpg";
+				img16.src = "img/alphabet/pictures/p.jpg";
+				img17.src = "img/alphabet/pictures/q.jpg";
+				img18.src = "img/alphabet/pictures/r.jpg";
+				img19.src = "img/alphabet/pictures/s.jpg";
+				img20.src = "img/alphabet/pictures/t.jpg";
+				img21.src = "img/alphabet/pictures/u.jpg";
+				img22.src = "img/alphabet/pictures/v.jpg";
+				img23.src = "img/alphabet/pictures/w.jpg";
+				img24.src = "img/alphabet/pictures/x.jpg";
+				img25.src = "img/alphabet/pictures/y.jpg";
+				img26.src = "img/alphabet/pictures/z.jpg";
+
+				img_1.src = "img/numbers/picture/1.jpg";
+				img_2.src = "img/numbers/picture/2.jpg";
+				img_3.src = "img/numbers/picture/3.jpg";
+				img_4.src = "img/numbers/picture/4.jpg";
+				img_5.src = "img/numbers/picture/5.jpg";
+				img_6.src = "img/numbers/picture/6.jpg";
+				img_7.src = "img/numbers/picture/7.jpg";
+				img_8.src = "img/numbers/picture/8.jpg";
+				img_9.src = "img/numbers/picture/9.jpg";
+				img_10.src = "img/numbers/picture/10.jpg";
+
+			}
+
+		
+	</script>
